8450e6372795b08bf21623cb5337891291a6cab9
Stijn Deridder <stijn_deridder@hotmailcom>
2015-01-23 12:49:50 +0100

    writeline toevoegen

--- GitHub_HelloWorld/Program.cs
+++ GitHub_HelloWorld/Program.cs
@@ -13,6 +13,7 @@ namespace GitHub_HelloWorld
             string tekst = "ik ben tarik";
             Console.WriteLine("nee ik ben jordy");
             Console.WriteLine("Ma mannekes toch, kindjes :/");
+            Console.WriteLine("Gelukkige verjaardag eh manneke");
             Console.ReadLine();
         }
     }
